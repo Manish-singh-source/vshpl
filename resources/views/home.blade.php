42a1044619ae58a1c3aa52063eede3fe9b9ab005
manish <support@technofra.com>
2026-01-16 14:55:23 +0530

    add newyhtftdfhfd

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>VSH Cricket Premier League</title>
+    <title>VEENA SMART HOMES</title>
     <link rel="icon" type="image/x-icon" href="{{ asset('assets/favicon.png') }}">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@700&display=swap" rel="stylesheet">
     <style>
