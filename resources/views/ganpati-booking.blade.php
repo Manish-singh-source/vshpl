51a695c0773428b71579ccfaff887186fc9a5a94
manish <support@technofra.com>
2026-07-24 17:47:18 +0530

    dfsdgsgdfgwgyuohujo

--- resources/views/ganpati-booking.blade.php
+++ resources/views/ganpati-booking.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Veena Smart Homes | Ganesh Utsav Celebration</title>
-    <link rel="icon" type="image/x-icon" href="{{ asset('assets/main.png') }}">
+    <link rel="icon" type="image/x-icon" href="{{ asset('assets/main2.png') }}">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Cinzel:wght@500;700&family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
